880d465cd84e4fcf8fb94e54cd31138a1d42b50f
rubenpulido <ruben.pulido@liferay.com>
2018-11-23 12:48:54 +0100

    LPS-85972 Add Expression implementations

---
+++ modules/apps/portal-odata/portal-odata-impl/src/main/java/com/liferay/portal/odata/internal/filter/expression/ComplexPropertyExpressionImpl.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.odata.internal.filter.expression;
+
+import com.liferay.portal.odata.filter.expression.ComplexPropertyExpression;
+import com.liferay.portal.odata.filter.expression.ExpressionVisitException;
+import com.liferay.portal.odata.filter.expression.ExpressionVisitor;
+
+/**
+ * @author Ruben Pulido
+ */
+public class ComplexPropertyExpressionImpl
+	implements ComplexPropertyExpression {
+
+	public ComplexPropertyExpressionImpl(String name) {
+		_name = name;
+	}
+
+	@Override
+	public <T> T accept(ExpressionVisitor<T> expressionVisitor)
+		throws ExpressionVisitException {
+
+		return expressionVisitor.visitComplexPropertyExpression(this);
+	}
+
+	@Override
+	public String getName() {
+		return _name;
+	}
+
+	private final String _name;
+
+}
