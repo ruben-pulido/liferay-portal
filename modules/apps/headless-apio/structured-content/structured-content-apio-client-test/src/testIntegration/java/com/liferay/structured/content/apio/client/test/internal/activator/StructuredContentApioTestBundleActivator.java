6fd95792440ae5f41af8e0f123877e9a53b6d485
rubenpulido <ruben.pulido@liferay.com>
2018-12-21 09:17:18 +0100

    LPS-88163 Private packages must be under internal. Apply to all modules.

--- modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/internal/activator/StructuredContentApioTestBundleActivator.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/internal/activator/StructuredContentApioTestBundleActivator.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.structured.content.apio.client.test.activator;
+package com.liferay.structured.content.apio.client.test.internal.activator;
 
 import com.liferay.dynamic.data.mapping.io.DDMFormDeserializer;
 import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerDeserializeRequest;
@@ -213,8 +213,8 @@ public class StructuredContentApioTestBundleActivator
 		ClassLoader classLoader = clazz.getClassLoader();
 
 		InputStream inputStream = classLoader.getResourceAsStream(
-			"/com/liferay/structured/content/apio/client/test/activator/" +
-				fileName);
+			"/com/liferay/structured/content/apio/client/test/internal" +
+				"/activator/" + fileName);
 
 		return StringUtil.read(inputStream);
 	}
