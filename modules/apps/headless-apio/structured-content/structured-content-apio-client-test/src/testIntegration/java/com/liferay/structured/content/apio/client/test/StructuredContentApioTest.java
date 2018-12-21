4aededad1070f392b4475264f4acf461f60ed361
rubenpulido <ruben.pulido@liferay.com>
2018-12-21 12:12:12 +0100

    LPS-88163 Private packages must be under internal. Apply to all modules.

--- modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
@@ -19,7 +19,7 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.apio.test.util.ApioClientBuilder;
 import com.liferay.portal.apio.test.util.ContentSpaceApioTestUtil;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.structured.content.apio.client.test.activator.StructuredContentApioTestBundleActivator;
+import com.liferay.structured.content.apio.client.test.internal.activator.StructuredContentApioTestBundleActivator;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -187,7 +187,7 @@ public class StructuredContentApioTest {
 			structuredContentHref
 		).then(
 		).statusCode(
-			Matchers.isOneOf(200, 204)
+			200
 		);
 	}
 
