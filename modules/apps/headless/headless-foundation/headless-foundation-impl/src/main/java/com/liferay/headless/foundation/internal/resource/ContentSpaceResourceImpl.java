62863a3ad044d37a937e649d084a77d013fad6f4
Sarai Díaz García <dgarciasarai@gmail.com>
2019-01-29 12:44:21 +0100

    LPS-89768 Get content spaces collection

--- modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/ContentSpaceResourceImpl.java
+++ modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/ContentSpaceResourceImpl.java
@@ -17,13 +17,23 @@ package com.liferay.headless.foundation.internal.resource;
 import com.liferay.headless.foundation.dto.ContentSpace;
 import com.liferay.headless.foundation.dto.ContentSpaceCollection;
 import com.liferay.headless.foundation.resource.ContentSpaceResource;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.CompanyService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.comparator.GroupIdComparator;
 import com.liferay.portal.vulcan.context.Pagination;
 
-import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.annotation.Generated;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
 
@@ -46,7 +56,37 @@ public class ContentSpaceResourceImpl implements ContentSpaceResource {
 			Pagination pagination, String size)
 		throws Exception {
 
-		return new ContentSpaceCollection(Collections.emptyList(), 0);
+		Company company = _companyService.getCompanyByWebId(
+			PropsUtil.get(PropsKeys.COMPANY_DEFAULT_WEB_ID));
+
+		List<Group> groups = _groupLocalService.getActiveGroups(
+			company.getCompanyId(), true, true, pagination.getStartPosition(),
+			pagination.getEndPosition(), new GroupIdComparator(true));
+
+		Stream<Group> stream = groups.stream();
+
+		List<ContentSpace> contentSpaces = stream.map(
+			group -> {
+				ContentSpace contentSpace = new ContentSpace();
+
+				contentSpace.setId(group.getGroupId());
+
+				return contentSpace;
+			}
+		).collect(
+			Collectors.toList()
+		);
+
+		int totalCount = _groupLocalService.getActiveGroupsCount(
+			company.getCompanyId(), true, true);
+
+		return new ContentSpaceCollection(contentSpaces, totalCount);
 	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private CompanyService _companyService;
 
 }
