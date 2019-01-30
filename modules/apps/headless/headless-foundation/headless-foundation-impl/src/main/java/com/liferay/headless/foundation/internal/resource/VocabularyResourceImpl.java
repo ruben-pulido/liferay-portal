227ee1448e3dd70f75c053202d7c69f16cf13dc8
Sarai Díaz García <dgarciasarai@gmail.com>
2019-01-30 15:58:49 +0100

    LPS-89820 Get vocabulary collection

--- modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/VocabularyResourceImpl.java
+++ modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/VocabularyResourceImpl.java
@@ -14,16 +14,26 @@
 
 package com.liferay.headless.foundation.internal.resource;
 
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetVocabularyService;
 import com.liferay.headless.foundation.dto.Vocabulary;
 import com.liferay.headless.foundation.dto.VocabularyCollection;
 import com.liferay.headless.foundation.resource.VocabularyResource;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.CompanyService;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.vulcan.context.Pagination;
 
-import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.annotation.Generated;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
 
@@ -46,7 +56,40 @@ public class VocabularyResourceImpl implements VocabularyResource {
 			Pagination pagination, String size)
 		throws Exception {
 
-		return new VocabularyCollection(Collections.emptyList(), 0);
+		Company company = _companyService.getCompanyByWebId(
+			PropsUtil.get(PropsKeys.COMPANY_DEFAULT_WEB_ID));
+
+		Group group = company.getGroup();
+
+		List<AssetVocabulary> assetVocabularies =
+			_assetVocabularyService.getGroupVocabularies(
+				group.getGroupId(), pagination.getStartPosition(),
+				pagination.getEndPosition(), null);
+
+		int count = _assetVocabularyService.getGroupVocabulariesCount(
+			group.getGroupId());
+
+		Stream<AssetVocabulary> stream = assetVocabularies.stream();
+
+		List<Vocabulary> vocabularies = stream.map(
+			assetVocabulary -> {
+				Vocabulary vocabulary = new Vocabulary();
+
+				vocabulary.setId(assetVocabulary.getVocabularyId());
+
+				return vocabulary;
+			}
+		).collect(
+			Collectors.toList()
+		);
+
+		return new VocabularyCollection(vocabularies, count);
 	}
+
+	@Reference
+	private AssetVocabularyService _assetVocabularyService;
+
+	@Reference
+	private CompanyService _companyService;
 
 }
