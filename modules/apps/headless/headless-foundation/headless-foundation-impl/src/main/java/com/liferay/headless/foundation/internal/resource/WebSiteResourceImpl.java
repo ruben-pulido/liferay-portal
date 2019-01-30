ead77649507b0738f02a9899a4ca8413f7c5985d
Sarai Díaz García <dgarciasarai@gmail.com>
2019-01-30 16:39:16 +0100

    LPS-89776 Get webSite collection

--- modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/WebSiteResourceImpl.java
+++ modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/WebSiteResourceImpl.java
@@ -17,13 +17,23 @@ package com.liferay.headless.foundation.internal.resource;
 import com.liferay.headless.foundation.dto.WebSite;
 import com.liferay.headless.foundation.dto.WebSiteCollection;
 import com.liferay.headless.foundation.resource.WebSiteResource;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.CompanyService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.comparator.GroupIdComparator;
 import com.liferay.portal.vulcan.context.Pagination;
 
-import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.annotation.Generated;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
 
@@ -46,7 +56,37 @@ public class WebSiteResourceImpl implements WebSiteResource {
 			Pagination pagination, String size)
 		throws Exception {
 
-		return new WebSiteCollection(Collections.emptyList(), 0);
+		Company company = _companyService.getCompanyByWebId(
+			PropsUtil.get(PropsKeys.COMPANY_DEFAULT_WEB_ID));
+
+		List<Group> groups = _groupLocalService.getActiveGroups(
+			company.getCompanyId(), true, true, pagination.getStartPosition(),
+			pagination.getEndPosition(), new GroupIdComparator(true));
+
+		Stream<Group> stream = groups.stream();
+
+		List<WebSite> webSites = stream.map(
+			group -> {
+				WebSite webSite = new WebSite();
+
+				webSite.setId(group.getGroupId());
+
+				return webSite;
+			}
+		).collect(
+			Collectors.toList()
+		);
+
+		int count = _groupLocalService.getActiveGroupsCount(
+			company.getCompanyId(), true, true);
+
+		return new WebSiteCollection(webSites, count);
 	}
+
+	@Reference
+	private CompanyService _companyService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
 
 }
