54296e967cc136bc06d7664e7df629c9935e297f
rubenpulido <ruben.pulido@liferay.com>
2019-01-30 16:21:38 +0100

    LPS-89787 Implement getCategoryCollection

--- modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/CategoryResourceImpl.java
+++ modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/CategoryResourceImpl.java
@@ -14,16 +14,29 @@
 
 package com.liferay.headless.foundation.internal.resource;
 
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetCategoryService;
+import com.liferay.asset.kernel.service.AssetVocabularyService;
 import com.liferay.headless.foundation.dto.Category;
 import com.liferay.headless.foundation.dto.CategoryCollection;
 import com.liferay.headless.foundation.resource.CategoryResource;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.CompanyService;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.vulcan.context.Pagination;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.annotation.Generated;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
 
@@ -46,7 +59,53 @@ public class CategoryResourceImpl implements CategoryResource {
 			Pagination pagination, String size)
 		throws Exception {
 
-		return new CategoryCollection(Collections.emptyList(), 0);
+		Company company = _companyService.getCompanyByWebId(
+			PropsUtil.get(PropsKeys.COMPANY_DEFAULT_WEB_ID));
+
+		Group group = company.getGroup();
+
+		List<AssetVocabulary> assetVocabularies =
+			_assetVocabularyService.getGroupVocabularies(group.getGroupId());
+
+		if (assetVocabularies.isEmpty()) {
+			return new CategoryCollection(Collections.emptyList(), 0);
+		}
+
+		AssetVocabulary assetVocabulary = assetVocabularies.get(0);
+
+		List<AssetCategory> assetCategories =
+			_assetCategoryService.getVocabularyRootCategories(
+				assetVocabulary.getGroupId(), assetVocabulary.getVocabularyId(),
+				pagination.getStartPosition(), pagination.getEndPosition(),
+				null);
+
+		Stream<AssetCategory> stream = assetCategories.stream();
+
+		List<Category> categories = stream.map(
+			assetCategory -> {
+				Category category = new Category();
+
+				category.setId(assetCategory.getCategoryId());
+
+				return category;
+			}
+		).collect(
+			Collectors.toList()
+		);
+
+		int count = _assetCategoryService.getVocabularyRootCategoriesCount(
+			assetVocabulary.getGroupId(), assetVocabulary.getVocabularyId());
+
+		return new CategoryCollection(categories, count);
 	}
+
+	@Reference
+	private AssetCategoryService _assetCategoryService;
+
+	@Reference
+	private AssetVocabularyService _assetVocabularyService;
+
+	@Reference
+	private CompanyService _companyService;
 
 }
