0d7e253c3d787acf8839184e1c7452154bb4df29
rubenpulido <ruben.pulido@liferay.com>
2019-06-05 15:47:08 +0200

    LPS-96213 WIP - AssetDisplayPageHelperTest

--- modules/apps/asset/asset-display-page-test/src/testIntegration/java/com/liferay/asset/display/page/util/AssetDisplayPageHelperTest.java
+++ modules/apps/asset/asset-display-page-test/src/testIntegration/java/com/liferay/asset/display/page/util/AssetDisplayPageHelperTest.java
@@ -1,21 +1,74 @@
 package com.liferay.asset.display.page.util;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.kernel.model.DLFileEntryConstants;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.kernel.model.Group;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
+@RunWith(Arquillian.class)
+/**
+ * @author Rubén Pulido
+ */
 public class AssetDisplayPageHelperTest {
 
-	@Test
-	public void getAssetDisplayPageLayoutPageTemplateEntry() {
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+	@DeleteAfterTestRun
+	private Group _group;
+	@Inject
+	private LayoutPageTemplateEntryLocalService
+		_layoutPageTemplateEntryLocalService;
 
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
 	}
 
 	@Test
-	public void hasAssetDisplayPage() {
+	public void testHasAssetDisplayPageForAssetWithSpecificDisplayPage() {
+		JournalArticle journalArticle = JournalTestUtil.addArticle(
+			stagingGroup.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+		AssetDisplayPageHelper.hasAssetDisplayPage()
 	}
 
 	@Test
-	public void hasAssetDisplayPage1() {
+	public void testHasAssetDisplayPageForAssetWithNoDisplayPage() {
+	}
+
+	@Test
+	public void testHasAssetDisplayPageForAssetWithExistingDefaultDisplayPage() {
+		long classNameId = _portal.getClassNameId(
+			DLFileEntryConstants.getClassName());
+
+		LayoutPageTemplateEntry defaultLayoutPageTemplateEntry =
+			_layoutPageTemplateEntryLocalService.addLayoutPageTemplateEntry(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				classNameId, 0, RandomTestUtil.randomString(),
+				LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE, true, 0,
+				0, WorkflowConstants.STATUS_APPROVED, new ServiceContext());
+	}
+
+	@Test
+	public void testHasAssetDisplayPageForAssetWithNonexistingDisplayPage() {
 	}
 }
