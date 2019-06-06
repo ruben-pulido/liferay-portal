83069ba25506ff81b0de257cd9e6e4e686f0ce67
rubenpulido <ruben.pulido@liferay.com>
2019-06-06 09:35:07 +0200

    LPS-95343 Create GroupNameException
    Co-authored-by: Víctor Galán <victor.galan@liferay.com>

---
+++ portal-kernel/src/com/liferay/portal/kernel/exception/GroupNameException.java
@@ -0,0 +1,23 @@
+package com.liferay.portal.kernel.exception;
+
+/**
+ * @author Rubén Pulido
+ */
+public class GroupNameException extends PortalException {
+
+	public GroupNameException() {
+	}
+
+	public GroupNameException(String msg) {
+		super(msg);
+	}
+
+	public GroupNameException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public GroupNameException(Throwable cause) {
+		super(cause);
+	}
+
+}
