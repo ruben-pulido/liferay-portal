1eb4e67f1f0d661b155dab535c9bbdf0d2d3dc03
rubenpulido <ruben.pulido@liferay.com>
2019-07-09 01:00:51 +0200

    LPS-97744 Unit tests for FragmentEntryConfigUtil.getConfigurationValuesJSONObject

--- modules/apps/fragment/fragment-api/src/test/java/com/liferay/fragment/util/FragmentEntryConfigUtilTest.java
+++ modules/apps/fragment/fragment-api/src/test/java/com/liferay/fragment/util/FragmentEntryConfigUtilTest.java
@@ -14,11 +14,13 @@
 
 package com.liferay.fragment.util;
 
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.json.JSONFactoryImpl;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.util.FileImpl;
+import com.liferay.segments.constants.SegmentsConstants;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -48,11 +50,363 @@ public class FragmentEntryConfigUtilTest {
 
 		JSONObject expectedConfigurationDefaultValuesJSONObject =
 			JSONFactoryUtil.createJSONObject(
-				_getFileContent("expected-configuration-default-values.json"));
+				_getFileContent("expected-configuration-values-default.json"));
 
 		Assert.assertEquals(
 			expectedConfigurationDefaultValuesJSONObject.toJSONString(),
 			configurationDefaultValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectBlank() throws Exception {
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"), StringPool.BLANK,
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectConfigBlank()
+		throws Exception {
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				StringPool.BLANK,
+				_getFileContent("editable-values-default.json"),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals("{}", configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectConfigNull()
+		throws Exception {
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				null, _getFileContent("editable-values-default.json"),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals("{}", configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectDefault() throws Exception {
+		String fileName = "editable-values-default.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectEmtpy() throws Exception {
+		String fileName = "editable-values-empty.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldInvalidDataType()
+		throws Exception {
+
+		String fileName = "editable-values-field-invalid-data-type.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldInvalidName()
+		throws Exception {
+
+		String fileName = "editable-values-field-invalid-name.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldInvalidOption()
+		throws Exception {
+
+		String fileName = "editable-values-field-invalid-option.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldMissingNondefaultInvalidOptionNullInvalidName()
+		throws Exception {
+
+		String fileName =
+			"editable-values-field-missing-non-default-invalid-option-null-" +
+				"invalid-name.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent(
+					"expected-configuration-values-field-missing-non-default-" +
+						"invalid-option-null-invalid-name.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldNondefault()
+		throws Exception {
+
+		String fileName = "editable-values-field-non-default.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent(
+					"expected-configuration-values-field-non-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldNull()
+		throws Exception {
+
+		String fileName = "editable-values-field-null.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldSegmentExperienceId0Empty()
+		throws Exception {
+
+		String fileName = "editable-values-segments-experience-id-0-empty.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldSegmentExperienceIdInvalid()
+		throws Exception {
+
+		String fileName = "editable-values-default.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName), 10);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldSetEmpty()
+		throws Exception {
+
+		String fileName = "editable-values-fieldSet-empty.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldSetInvalidJSON()
+		throws Exception {
+
+		String fileName = "editable-values-fieldSet-invalid-json.json.txt";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFieldSetInvalidName()
+		throws Exception {
+
+		String fileName = "editable-values-fieldSet-invalid-name.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectFreeMarkerFragmentEntryProcessorEmpty()
+		throws Exception {
+
+		String fileName =
+			"editable-values-FreeMarkerFragmentEntryProcessor-empty.json";
+
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"),
+				_getFileContent(fileName),
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
+	}
+
+	@Test
+	public void testGetConfigurationValuesJSONObjectNull() throws Exception {
+		JSONObject configurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-values-default.json"));
+
+		JSONObject configurationValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationValuesJSONObject(
+				_getFileContent("configuration.json"), null,
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		Assert.assertEquals(
+			configurationDefaultValuesJSONObject.toJSONString(),
+			configurationValuesJSONObject.toJSONString());
 	}
 
 	private String _getFileContent(String fileName) throws Exception {
