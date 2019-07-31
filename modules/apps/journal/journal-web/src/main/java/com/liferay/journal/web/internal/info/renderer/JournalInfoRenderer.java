67b1d279be0100387df0dfdff51dbae78d41099d
rubenpulido <ruben.pulido@liferay.com>
2019-07-31 11:43:22 +0200

    LPS-99068 WIP - SAMPLE DATA - JournalInfoRenderer

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/renderer/JournalInfoRenderer.java
@@ -0,0 +1,42 @@
+package com.liferay.journal.web.internal.info.renderer;
+
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.info.renderer.InfoItemRenderer;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.util.WebKeys;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component(service = InfoItemRenderer.class)
+public class JournalInfoRenderer implements InfoItemRenderer<JournalArticle> {
+
+	@Override
+	public void render(
+		JournalArticle journalArticle, HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+
+		AssetRendererFactory<?> assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
+				JournalArticle.class.getName());
+
+		httpServletRequest.setAttribute(
+			WebKeys.ASSET_RENDERER_FACTORY, assetRendererFactory);
+
+		try {
+			AssetRenderer<?> assetRenderer =
+				assetRendererFactory.getAssetRenderer(
+					journalArticle.getResourcePrimKey());
+
+			assetRenderer.include(
+				httpServletRequest, httpServletResponse,
+				AssetRenderer.TEMPLATE_FULL_CONTENT);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
