5131aed84ed33837471c93a658f590455102b880
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-02-13 14:36:22 +0100

    LPS-107756 We need to normalize the key for page template collection + entry, so create a new util class and related method for this

---
+++ modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/util/LayoutPageTemplateNormalizerUtil.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.util;
+
+import com.liferay.petra.string.StringPool;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class LayoutPageTemplateNormalizerUtil {
+
+	public static String normalizeModelKey(String str) {
+		Matcher matcher = _pattern.matcher(str);
+
+		return matcher.replaceAll(StringPool.DASH);
+	}
+
+	private static final Pattern _pattern = Pattern.compile("[^\\w-]");
+
+}
