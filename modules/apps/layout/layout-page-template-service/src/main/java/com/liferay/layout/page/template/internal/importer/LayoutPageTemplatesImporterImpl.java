f53a05bc6a32d367210e080e5da6461925925d4d
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-02-17 12:51:05 +0100

    LPS-107756 Scan the zip file for valid entries and populate the map for future use

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/importer/LayoutPageTemplatesImporterImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/importer/LayoutPageTemplatesImporterImpl.java
@@ -15,8 +15,20 @@
 package com.liferay.layout.page.template.internal.importer;
 
 import com.liferay.layout.page.template.importer.LayoutPageTemplatesImporter;
+import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.File;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -31,6 +43,96 @@ public class LayoutPageTemplatesImporterImpl
 	public void importFile(
 			long userId, long groupId, File file, boolean overwrite)
 		throws Exception {
+
+		try (ZipFile zipFile = new ZipFile(file)) {
+			Map<String, LayoutPageTemplateCollectionFolder>
+				layoutPageTemplateCollectionFolderMap =
+					_getLayoutPageTemplateCollectionFolderMap(zipFile);
+
+			if (MapUtil.isEmpty(layoutPageTemplateCollectionFolderMap)) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(
+						"No valid layout page template entries found in " +
+							zipFile.getName());
+				}
+			}
+		}
+	}
+
+	private Map<String, LayoutPageTemplateCollectionFolder>
+		_getLayoutPageTemplateCollectionFolderMap(ZipFile zipFile) {
+
+		Map<String, LayoutPageTemplateCollectionFolder>
+			layoutPageTemplateCollectionFolderMap = new HashMap<>();
+
+		Enumeration<? extends ZipEntry> enumeration = zipFile.entries();
+
+		while (enumeration.hasMoreElements()) {
+			ZipEntry zipEntry = enumeration.nextElement();
+
+			if (zipEntry.isDirectory()) {
+				continue;
+			}
+
+			String fileName = zipEntry.getName();
+
+			String[] pathParameters = StringUtil.split(
+				fileName, CharPool.SLASH);
+
+			if ((pathParameters.length != 4) ||
+				!Objects.equals(pathParameters[0], _ROOT_FOLDER) ||
+				!Objects.equals(pathParameters[3], _PAGE_TEMPLATE_FILE_NAME)) {
+
+				continue;
+			}
+
+			String layoutPageTemplateCollectionKey = pathParameters[1];
+
+			String layoutPageTemplateEntryKey = pathParameters[2];
+
+			LayoutPageTemplateCollectionFolder
+				layoutPageTemplateCollectionFolder =
+					layoutPageTemplateCollectionFolderMap.computeIfAbsent(
+						layoutPageTemplateCollectionKey,
+						key -> new LayoutPageTemplateCollectionFolder(key));
+
+			layoutPageTemplateCollectionFolder.addLayoutPageTemplateEntry(
+				layoutPageTemplateEntryKey, fileName);
+
+			layoutPageTemplateCollectionFolderMap.put(
+				layoutPageTemplateCollectionKey,
+				layoutPageTemplateCollectionFolder);
+		}
+
+		return layoutPageTemplateCollectionFolderMap;
+	}
+
+	private static final String _PAGE_TEMPLATE_FILE_NAME = "page-template.json";
+
+	private static final String _ROOT_FOLDER = "page-templates";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutPageTemplatesImporterImpl.class);
+
+	private class LayoutPageTemplateCollectionFolder {
+
+		public LayoutPageTemplateCollectionFolder(String key) {
+			_key = key;
+
+			_layoutPageTemplateEntries = new HashMap<>();
+		}
+
+		public void addLayoutPageTemplateEntry(String key, String fileName) {
+			_layoutPageTemplateEntries.put(key, fileName);
+		}
+
+		public String getKey() {
+			return _key;
+		}
+
+		private final String _key;
+		private final Map<String, String> _layoutPageTemplateEntries;
+
 	}
 
 }
