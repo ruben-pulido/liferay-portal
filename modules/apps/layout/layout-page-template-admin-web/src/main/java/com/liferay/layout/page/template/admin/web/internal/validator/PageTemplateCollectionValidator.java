b2325fad5ed5df472603e19800d9ac7c0b9e4abf
Rubén Pulido <ruben.pulido@liferay.com>
2020-02-18 14:39:03 +0100

    LPS-108873 Create PageTemplateCollectionValidator

---
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/validator/PageTemplateCollectionValidator.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.admin.web.internal.validator;
+
+import com.liferay.layout.page.template.admin.web.internal.exception.PageTemplateCollectionValidatorException;
+import com.liferay.petra.json.validator.JSONValidator;
+import com.liferay.petra.json.validator.JSONValidatorException;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.io.InputStream;
+
+/**
+ * @author Rubén Pulido
+ */
+public class PageTemplateCollectionValidator {
+
+	public static void validatePageTemplateCollection(
+			String pageTemplateCollection)
+		throws PageTemplateCollectionValidatorException {
+
+		if (Validator.isNull(pageTemplateCollection)) {
+			return;
+		}
+
+		InputStream pageTemplateCollectionJSONSchemaInputStream =
+			PageTemplateCollectionValidator.class.getResourceAsStream(
+				"dependencies/page_template_collection_json_schema.json");
+
+		try {
+			JSONValidator.validate(
+				pageTemplateCollection,
+				pageTemplateCollectionJSONSchemaInputStream);
+		}
+		catch (JSONValidatorException jsonValidatorException) {
+			throw new PageTemplateCollectionValidatorException(
+				jsonValidatorException.getMessage(), jsonValidatorException);
+		}
+	}
+
+}
