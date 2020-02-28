f4cdcca7cb39f15868114a8b17034dbc91b3d363
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-02-28 09:11:46 +0100

    LPS-107757 Adds logic to create a fragmentEntryLink and return the fragmentLayoutStructureItem for it

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/importer/helper/FragmentLayoutStructureItemHelper.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/importer/helper/FragmentLayoutStructureItemHelper.java
@@ -15,10 +15,24 @@
 package com.liferay.layout.page.template.internal.importer.helper;
 
 import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalServiceUtil;
+import com.liferay.fragment.service.FragmentEntryLocalServiceUtil;
+import com.liferay.headless.delivery.dto.v1_0.Fragment;
+import com.liferay.headless.delivery.dto.v1_0.FragmentInstanceDefinition;
 import com.liferay.headless.delivery.dto.v1_0.PageElement;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.layout.util.structure.LayoutStructureItem;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 
 /**
  * @author Jürgen Kappler
@@ -34,7 +48,90 @@ public class FragmentLayoutStructureItemHelper
 		LayoutStructure layoutStructure, PageElement pageElement,
 		String parentItemId, int position) {
 
+		FragmentEntryLink fragmentEntryLink = _addFragmentEntryLink(
+			fragmentCollectionContributorTracker, layoutPageTemplateEntry,
+			pageElement, position);
+
+		if (fragmentEntryLink == null) {
+			return null;
+		}
+
+		return layoutStructure.addFragmentLayoutStructureItem(
+			fragmentEntryLink.getFragmentEntryLinkId(), parentItemId, position);
+	}
+
+	private FragmentEntryLink _addFragmentEntryLink(
+		FragmentCollectionContributorTracker
+			fragmentCollectionContributorTracker,
+		LayoutPageTemplateEntry layoutPageTemplateEntry,
+		PageElement pageElement, int position) {
+
+		FragmentInstanceDefinition fragmentInstanceDefinition =
+			(FragmentInstanceDefinition)pageElement.getDefinition();
+
+		if (fragmentInstanceDefinition == null) {
+			return null;
+		}
+
+		Fragment fragment = fragmentInstanceDefinition.getFragment();
+
+		FragmentEntry fragmentEntry = _getFragmentEntry(
+			fragmentCollectionContributorTracker, fragment.getFragmentKey(),
+			layoutPageTemplateEntry);
+
+		long fragmentEntryId = 0;
+		String html = StringPool.BLANK;
+		String js = StringPool.BLANK;
+		String css = StringPool.BLANK;
+		String configuration = StringPool.BLANK;
+
+		if (fragmentEntry != null) {
+			fragmentEntryId = fragmentEntry.getFragmentEntryId();
+
+			html = fragmentEntry.getHtml();
+			js = fragmentEntry.getJs();
+			css = fragmentEntry.getCss();
+			configuration = fragmentEntry.getConfiguration();
+		}
+
+		try {
+			return FragmentEntryLinkLocalServiceUtil.addFragmentEntryLink(
+				layoutPageTemplateEntry.getUserId(),
+				layoutPageTemplateEntry.getGroupId(), 0, fragmentEntryId,
+				PortalUtil.getClassNameId(Layout.class.getName()),
+				layoutPageTemplateEntry.getPlid(), css, html, js, configuration,
+				StringPool.BLANK, StringUtil.randomId(), position,
+				fragment.getFragmentKey(),
+				ServiceContextThreadLocal.getServiceContext());
+		}
+		catch (PortalException portalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(portalException, portalException);
+			}
+		}
+
 		return null;
 	}
+
+	private FragmentEntry _getFragmentEntry(
+		FragmentCollectionContributorTracker
+			fragmentCollectionContributorTracker,
+		String fragmentKey, LayoutPageTemplateEntry layoutPageTemplateEntry) {
+
+		FragmentEntry fragmentEntry =
+			FragmentEntryLocalServiceUtil.fetchFragmentEntry(
+				layoutPageTemplateEntry.getGroupId(), fragmentKey);
+
+		if (fragmentEntry == null) {
+			fragmentEntry =
+				fragmentCollectionContributorTracker.getFragmentEntry(
+					fragmentKey);
+		}
+
+		return fragmentEntry;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		FragmentLayoutStructureItemHelper.class);
 
 }
