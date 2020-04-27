53f1720500226f63aded4475c33c5747ef415659
Rubén Pulido <ruben.pulido@liferay.com>
2020-04-27 18:46:01 +0200

    LPS-111660 Don't export display page template in draft status

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/helper/ExportHelper.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/helper/ExportHelper.java
@@ -78,6 +78,10 @@ public class ExportHelper {
 			for (LayoutPageTemplateEntry layoutPageTemplateEntry :
 					layoutPageTemplateEntries) {
 
+				if (layoutPageTemplateEntry.isDraft()) {
+					continue;
+				}
+
 				_populateDisplayPagesZipWriter(
 					layoutPageTemplateEntry, zipWriter);
 			}
