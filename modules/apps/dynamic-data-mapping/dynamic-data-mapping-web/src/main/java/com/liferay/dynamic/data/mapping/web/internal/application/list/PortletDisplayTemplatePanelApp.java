e5a4685597f3e786c478255e1ce8e20d43abca0d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-07-03 10:29:22 +0100

    LPS-116205 Reorder panels

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/application/list/PortletDisplayTemplatePanelApp.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/application/list/PortletDisplayTemplatePanelApp.java
@@ -29,7 +29,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"panel.app.order:Integer=300",
+		"panel.app.order:Integer=500",
 		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_BUILD
 	},
 	service = PanelApp.class
--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/application/list/LayoutPageTemplatesPanelApp.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/application/list/LayoutPageTemplatesPanelApp.java
@@ -33,7 +33,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"panel.app.order:Integer=110",
+		"panel.app.order:Integer=200",
 		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_BUILD
 	},
 	service = PanelApp.class
