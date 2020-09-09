6099c9329f33dbcc7c6145789ee66b187e2ea571
Rubén Pulido <ruben.pulido@liferay.com>
2020-09-09 10:31:47 +0200

    CHESS-1 Add Chess Control Panel Category

---
+++ modules/apps/chess/chess-web/src/main/java/com/liferay/chess/web/internal/constants/ChessPortletKeys.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.chess.web.internal.constants;
+
+/**
+ * @author Rubén Pulido
+ */
+public class ChessPortletKeys {
+
+	public static final String CHESS =
+		"com_liferay_chess_web_internal_portlet_ChessPortlet";
+
+	public static final String CHESS_CONFIGURATION =
+		"com_liferay_chess_web_internal_portlet_ChessConfigurationPortlet";
+
+}
