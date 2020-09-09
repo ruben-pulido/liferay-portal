fbce9247e7f43d8611b37d8d82bc488937ce1bd5
Rubén Pulido <ruben.pulido@liferay.com>
2020-09-09 10:31:48 +0200

    CHESS-5 buildService

---
+++ modules/apps/chess/chess-service/src/main/java/com/liferay/chess/model/impl/ChessGameImpl.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.chess.model.impl;
+
+/**
+ * The extended model implementation for the ChessGame service. Represents a row in the &quot;ChessGame&quot; database table, with each column mapped to a property of this class.
+ *
+ * <p>
+ * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.chess.model.ChessGame</code> interface.
+ * </p>
+ *
+ * @author Rubén Pulido
+ */
+public class ChessGameImpl extends ChessGameBaseImpl {
+
+	/**
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. All methods that expect a chess game model instance should use the {@link com.liferay.chess.model.ChessGame} interface instead.
+	 */
+	public ChessGameImpl() {
+	}
+
+}
