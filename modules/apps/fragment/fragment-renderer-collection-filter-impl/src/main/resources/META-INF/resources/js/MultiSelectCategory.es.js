71c39c682c5cb7a3f88baf30ef70d79b95b54005
Victor Galan <victor.galan@liferay.com>
2020-12-03 16:51:40 +0100

    LPS-123643 Update MultiSelectCategory component

--- modules/apps/fragment/fragment-renderer-collection-filter-impl/src/main/resources/META-INF/resources/js/MultiSelectCategory.es.js
+++ modules/apps/fragment/fragment-renderer-collection-filter-impl/src/main/resources/META-INF/resources/js/MultiSelectCategory.es.js
@@ -14,44 +14,107 @@
 
 import ClayButton from '@clayui/button';
 import {ClayDropDownWithItems} from '@clayui/drop-down';
-import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import React, {useEffect, useState} from 'react';
 
-const MultiSelectCategory = () => {
-  const items = [
-	{
-	  checked: false,
-	  label: "Practice",
-	  onChange: () => {
-	  	alert("Practice changed");
-	  },
-	  type: "checkbox",
-	  value: "39128"
-	},
-	{
-	  checked: false,
-	  label: "Competition",
-	  onChange: () => {
-	  	alert("Competition changed");
-	  },
-	  type: "checkbox",
-	  value: "39131"
-	}
-  ];
-  const [categories, setCategories] = useState({"39128": false, "39131": false});
+export default function MultiSelectCategory({
+	assetCategories,
+	assetVocabularyLabel,
+	fragmentEntryLinkId,
+	selectedAssetCategoryIds: initialSelectedCategoryIds,
+}) {
+	const [selectedCategoryIds, setSelectedCategoryIds] = useState(() => {
+		return assetCategories
+			.filter((category) =>
+				initialSelectedCategoryIds.includes(category.id)
+			)
+			.map((category) => category.id);
+	});
 
-  return (
-    <ClayDropDownWithItems
-      footerContent={
-        <>
-          <ClayButton>{"Apply"}</ClayButton>
-        </>
-      }
-      items={items}
-      trigger={<ClayButton>{"Select"}</ClayButton>}
-    />
-  );
+	const [filteredCategories, setFilteredCategories] = useState(
+		assetCategories
+	);
+
+	const [searchValue, setSearchValue] = useState('');
+
+	useEffect(() => {
+		setFilteredCategories(
+			searchValue
+				? assetCategories.filter(
+						(category) =>
+							category.label
+								.toLowerCase()
+								.indexOf(searchValue.toLowerCase()) !== -1
+				  )
+				: assetCategories
+		);
+	}, [searchValue, assetCategories]);
+
+	const onSelectedClick = (selected, id) => {
+		if (selected) {
+			setSelectedCategoryIds([...selectedCategoryIds, id]);
+		}
+		else {
+			setSelectedCategoryIds(
+				selectedCategoryIds.filter((category) => category !== id)
+			);
+		}
+	};
+
+	const items = filteredCategories.map((category) => ({
+		...category,
+		checked: initialSelectedCategoryIds.includes(category.id),
+		onChange: (selected) => onSelectedClick(selected, category.id),
+		type: 'checkbox',
+	}));
+
+	const onApply = () => {
+		const queryParamName = `categoryId_${fragmentEntryLinkId}`;
+		const search = new URLSearchParams(window.location.search);
+
+		search.delete(queryParamName);
+
+		selectedCategoryIds.forEach((id) => {
+			search.append(queryParamName, id);
+		});
+
+		window.location.search = search;
+	};
+
+	return (
+		<div>
+			<p className="font-weight-semi-bold mb-1">{assetVocabularyLabel}</p>
+			<ClayDropDownWithItems
+				footerContent={
+					<ClayButton onClick={onApply} small>
+						{Liferay.Language.get('apply')}
+					</ClayButton>
+				}
+				items={items}
+				onSearchValueChange={setSearchValue}
+				searchValue={searchValue}
+				searchable={true}
+				trigger={
+					<ClayButton
+						className="dropdown-toggle form-control-select text-left"
+						displayType="secondary"
+					>
+						{Liferay.Language.get('select')}
+					</ClayButton>
+				}
+			/>
+		</div>
+	);
+}
+
+MultiSelectCategory.propTypes = {
+	assetCategories: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.string.isRequired,
+			label: PropTypes.number.isRequired,
+		}).isRequired
+	),
+	assetVocabularyLabel: PropTypes.string.isRequired,
+	fragmentEntryLinkId: PropTypes.string.isRequired,
+	selectedAssetCategoryIds: PropTypes.string.isRequired,
 };
-
-export default () => (
-	<MultiSelectCategory />
-);
