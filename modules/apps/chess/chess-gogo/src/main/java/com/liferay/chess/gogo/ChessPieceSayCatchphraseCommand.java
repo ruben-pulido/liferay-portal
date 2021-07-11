94cb5fc4623e4b19588cfc329b3cf974473ba155
Rubén Pulido <ruben.pulido@liferay.com>
2021-07-11 22:54:41 +0200

    CHESS-13 Add osgi command ChessPieceSayCatchphraseCommand
    g! chess:pieceSayCatchphrase
    nightrider says: I'm still thinking about my catchphrase.

---
+++ modules/apps/chess/chess-gogo/src/main/java/com/liferay/chess/gogo/ChessPieceSayCatchphraseCommand.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.chess.gogo;
+
+import com.liferay.chess.piece.ChessPiece;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rubén Pulido
+ */
+@Component(
+	property = {
+		"osgi.command.function=pieceSayCatchphrase", "osgi.command.scope=chess"
+	},
+	service = Object.class
+)
+public class ChessPieceSayCatchphraseCommand {
+
+	public ChessPiece getChessPiece() {
+		return _chessPiece;
+	}
+
+	public void pieceSayCatchphrase() {
+		ChessPiece chessPiece = getChessPiece();
+
+		System.out.println(
+			chessPiece.getName() + " says: " + chessPiece.getCathphrase());
+	}
+
+	@Reference
+	private volatile ChessPiece _chessPiece;
+
+}
