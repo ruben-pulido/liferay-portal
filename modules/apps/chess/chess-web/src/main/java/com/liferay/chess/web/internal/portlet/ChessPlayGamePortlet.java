07ddd3cc44782b96a1a7879ba295f62285155034
Rubén Pulido <ruben.pulido@liferay.com>
2021-07-11 22:54:37 +0200

    CHESS-8 Create ChessPlayGamePortlet with simple JSP

---
+++ modules/apps/chess/chess-web/src/main/java/com/liferay/chess/web/internal/portlet/ChessPlayGamePortlet.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.chess.web.internal.portlet;
+
+import com.liferay.chess.web.internal.constants.ChessPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Rubén Pulido
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Play Game",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/META-INF/resources/",
+		"javax.portlet.init-param.view-template=/chess_play_game/view.jsp",
+		"javax.portlet.name=" + ChessPortletKeys.CHESS_PLAY_GAME,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user"
+	},
+	service = Portlet.class
+)
+public class ChessPlayGamePortlet extends MVCPortlet {
+}
