9000b9aca7c51df46f490c5bc9ad2ed372ebae79
Rubén Pulido <ruben.pulido@liferay.com>
2021-07-29 01:44:34 +0200

    CHESS-31 Display pieces in ChessBoard according to piecePlacement

--- modules/apps/chess/chess-web/src/main/resources/META-INF/resources/js/chess_play_game/ChessBoard.js
+++ modules/apps/chess/chess-web/src/main/resources/META-INF/resources/js/chess_play_game/ChessBoard.js
@@ -12,9 +12,22 @@
  * details.
  */
 
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function ChessBoard() {
+function PieceImg({piecePlacement, square}) {
+	const piece = piecePlacement[square];
+
+	if (!piece) {
+		return null;
+	}
+
+	return (
+		<img src={`/o/chess-web/images/chess_pieces/cburnett/${piece}.svg`} />
+	);
+}
+
+export default function ChessBoard({piecePlacement}) {
 	return (
 		<div>
 			Chess Board
@@ -23,146 +36,210 @@ export default function ChessBoard() {
 					<tbody>
 						<tr>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/rook_black.svg" />
+								{PieceImg({piecePlacement, square: 'a8'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/knight_black.svg" />
+								{PieceImg({piecePlacement, square: 'b8'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/bishop_black.svg" />
+								{PieceImg({piecePlacement, square: 'c8'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/queen_black.svg" />
+								{PieceImg({piecePlacement, square: 'd8'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/king_black.svg" />
+								{PieceImg({piecePlacement, square: 'e8'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/bishop_black.svg" />
+								{PieceImg({piecePlacement, square: 'f8'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/knight_black.svg" />
+								{PieceImg({piecePlacement, square: 'g8'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/rook_black.svg" />
+								{PieceImg({piecePlacement, square: 'h8'})}
 							</td>
 						</tr>
 						<tr>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+								{PieceImg({piecePlacement, square: 'a7'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+								{PieceImg({piecePlacement, square: 'b7'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+								{PieceImg({piecePlacement, square: 'c7'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+								{PieceImg({piecePlacement, square: 'd7'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+								{PieceImg({piecePlacement, square: 'e7'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+								{PieceImg({piecePlacement, square: 'f7'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+								{PieceImg({piecePlacement, square: 'g7'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+								{PieceImg({piecePlacement, square: 'h7'})}
 							</td>
 						</tr>
 						<tr>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-						</tr>
-						<tr>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-						</tr>
-						<tr>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-						</tr>
-						<tr>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-							<td className="square-black"></td>
-							<td className="square-white"></td>
-						</tr>
-						<tr>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+								{PieceImg({piecePlacement, square: 'a6'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+								{PieceImg({piecePlacement, square: 'b6'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+								{PieceImg({piecePlacement, square: 'c6'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+								{PieceImg({piecePlacement, square: 'd6'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+								{PieceImg({piecePlacement, square: 'e6'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+								{PieceImg({piecePlacement, square: 'f6'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+								{PieceImg({piecePlacement, square: 'g6'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+								{PieceImg({piecePlacement, square: 'h6'})}
 							</td>
 						</tr>
 						<tr>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/rook_white.svg" />
+								{PieceImg({piecePlacement, square: 'a5'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/knight_white.svg" />
+								{PieceImg({piecePlacement, square: 'b5'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/bishop_white.svg" />
+								{PieceImg({piecePlacement, square: 'c5'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/queen_white.svg" />
+								{PieceImg({piecePlacement, square: 'd5'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/king_white.svg" />
+								{PieceImg({piecePlacement, square: 'e5'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/bishop_white.svg" />
+								{PieceImg({piecePlacement, square: 'f5'})}
 							</td>
 							<td className="square-black">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/knight_white.svg" />
+								{PieceImg({piecePlacement, square: 'g5'})}
 							</td>
 							<td className="square-white">
-								<img src="/o/chess-web/images/chess_pieces/cburnett/white/rook_white.svg" />
+								{PieceImg({piecePlacement, square: 'h5'})}
+							</td>
+						</tr>
+						<tr>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'a4'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'b4'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'c4'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'd4'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'e4'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'f4'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'g4'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'h4'})}
+							</td>
+						</tr>
+						<tr>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'a3'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'b3'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'c3'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'd3'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'e3'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'f3'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'g3'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'h3'})}
+							</td>
+						</tr>
+						<tr>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'a2'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'b2'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'c2'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'd2'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'e2'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'f2'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'g2'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'h2'})}
+							</td>
+						</tr>
+						<tr>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'a1'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'b1'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'c1'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'd1'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'e1'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'f1'})}
+							</td>
+							<td className="square-black">
+								{PieceImg({piecePlacement, square: 'g1'})}
+							</td>
+							<td className="square-white">
+								{PieceImg({piecePlacement, square: 'h1'})}
 							</td>
 						</tr>
 					</tbody>
@@ -171,3 +248,7 @@ export default function ChessBoard() {
 		</div>
 	);
 }
+
+ChessBoard.propTypes = {
+	piecePlacement: PropTypes.object.isRequired,
+};
