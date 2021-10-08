d463a60e6f9fda142ce1f84fc00dc3be238ee031
Rubén Pulido <ruben.pulido@liferay.com>
2021-10-08 10:38:42 +0200

    CHESS-26 Create ChessGameExceptionRequestHandler processing InvalidChessMoveException or Exception

---
+++ modules/apps/chess/chess-web/src/main/java/com/liferay/chess/web/internal/handler/ChessGameExceptionRequestHandler.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.chess.web.internal.handler;
+
+import com.liferay.chess.exception.InvalidChessMoveException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Rubén Pulido
+ */
+@Component(immediate = true, service = ChessGameExceptionRequestHandler.class)
+public class ChessGameExceptionRequestHandler {
+
+	public void handleException(
+			ActionRequest actionRequest, ActionResponse actionResponse,
+			Exception exception)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String errorMessage = "an-unexpected-error-occurred";
+
+		if (exception instanceof InvalidChessMoveException) {
+			errorMessage = "please-enter-a-valid-move";
+		}
+
+		JSONObject jsonObject = JSONUtil.put(
+			"error", LanguageUtil.get(themeDisplay.getRequest(), errorMessage));
+
+		JSONPortletResponseUtil.writeJSON(
+			actionRequest, actionResponse, jsonObject);
+	}
+
+}
