75a2bafc3f5290ba35baa134b3558990a7d75c98
Rubén Pulido <ruben.pulido@liferay.com>
2022-01-14 11:14:14 +0100

    LPS-144288 Create FragmentCollectionItemDescriptor and FragmentCollectionContributorItemDescriptor
    Co-authored-by: Pablo Molina <contact@pablomolina.me>

---
+++ modules/apps/fragment/fragment-collection-item-selector-web/src/main/java/com/liferay/fragment/collection/item/selector/web/internal/FragmentCollectionContributorItemDescriptor.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.collection.item.selector.web.internal;
+
+import com.liferay.fragment.collection.item.selector.criterion.FragmentCollectionItemSelectorCriterion;
+import com.liferay.fragment.contributor.FragmentCollectionContributor;
+import com.liferay.item.selector.ItemSelectorViewDescriptor;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.Locale;
+
+/**
+ * @author Rubén Pulido
+ */
+public class FragmentCollectionContributorItemDescriptor
+	implements ItemSelectorViewDescriptor.ItemDescriptor {
+
+	public FragmentCollectionContributorItemDescriptor(
+		FragmentCollectionContributor fragmentCollectionContributor,
+		FragmentCollectionItemSelectorCriterion
+			fragmentCollectionItemSelectorCriterion) {
+
+		_fragmentCollectionContributor = fragmentCollectionContributor;
+		_fragmentCollectionItemSelectorCriterion =
+			fragmentCollectionItemSelectorCriterion;
+	}
+
+	@Override
+	public String getIcon() {
+		return "documents-and-media";
+	}
+
+	@Override
+	public String getImageURL() {
+		return null;
+	}
+
+	@Override
+	public String getPayload() {
+		return JSONUtil.put(
+			"name", _fragmentCollectionContributor.getName()
+		).put(
+			"previewURL",
+			StringUtil.replace(
+				_fragmentCollectionItemSelectorCriterion.
+					getPreviewURLTemplate(),
+				StringPool.UNDERLINE, StringPool.UNDERLINE,
+				HashMapBuilder.put(
+					"fragmentCollectionKey",
+					_fragmentCollectionContributor.getFragmentCollectionKey()
+				).build())
+		).toString();
+	}
+
+	@Override
+	public String getSubtitle(Locale locale) {
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return _fragmentCollectionContributor.getName();
+	}
+
+	private final FragmentCollectionContributor _fragmentCollectionContributor;
+	private final FragmentCollectionItemSelectorCriterion
+		_fragmentCollectionItemSelectorCriterion;
+
+}
