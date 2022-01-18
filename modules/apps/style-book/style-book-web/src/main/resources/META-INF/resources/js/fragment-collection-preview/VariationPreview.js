7324c80e77eb8ab8f39c33a4d925c942aa31e650
Pablo Molina <contact@pablomolina.me>
2022-01-18 15:10:50 +0100

    LPS-145046 Create FragmentCollectionPreview component

---
+++ modules/apps/style-book/style-book-web/src/main/resources/META-INF/resources/js/fragment-collection-preview/VariationPreview.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {fetch} from 'frontend-js-web';
+import React, {useEffect, useState} from 'react';
+
+export function VariationPreview({
+	fragmentEntryKey,
+	label,
+	namespace,
+	previewURL,
+	variation,
+}) {
+	const [html, setHTML] = useState('');
+
+	useEffect(() => {
+		fetch(previewURL, {
+			body: {
+				[`${namespace}fragmentEntryKey`]: fragmentEntryKey,
+				[`${namespace}configuration`]: variation,
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				setHTML(data.html);
+			})
+			.catch((error) => {
+				console.error(error);
+				setHTML('ERROR');
+			});
+	}, [fragmentEntryKey, namespace, previewURL, variation]);
+
+	return (
+		<article>
+			<h5>{label}</h5>
+
+			<div dangerouslySetInnerHTML={{__html: html}} />
+		</article>
+	);
+}
