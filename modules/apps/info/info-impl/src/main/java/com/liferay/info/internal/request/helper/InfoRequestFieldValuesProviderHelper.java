4e59b6d9c4d8ba3ca15e4d02dedeb4909d0dbd55
Rubén Pulido <ruben.pulido@liferay.com>
2022-07-11 12:23:25 +0200

    LPS-157551 Filter out null InfoFieldValues

--- modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/helper/InfoRequestFieldValuesProviderHelper.java
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/helper/InfoRequestFieldValuesProviderHelper.java
@@ -107,9 +107,13 @@ public class InfoRequestFieldValuesProviderHelper {
 				(infoField.getInfoFieldType() instanceof FileInfoFieldType)) {
 
 				for (FileItem fileItem : multipartParameters) {
-					infoFieldValues.add(
+					InfoFieldValue<Object> infoFieldValue =
 						_getFileInfoFieldValue(
-							fileItem, groupId, infoField, themeDisplay));
+							fileItem, groupId, infoField, themeDisplay);
+
+					if (infoFieldValue != null) {
+						infoFieldValues.add(infoFieldValue);
+					}
 				}
 			}
 
@@ -121,9 +125,12 @@ public class InfoRequestFieldValuesProviderHelper {
 			}
 
 			for (String value : regularParameters) {
-				infoFieldValues.add(
-					_getInfoFieldValue(
-						infoField, themeDisplay.getLocale(), value));
+				InfoFieldValue<Object> infoFieldValue = _getInfoFieldValue(
+					infoField, themeDisplay.getLocale(), value);
+
+				if (infoFieldValue != null) {
+					infoFieldValues.add(infoFieldValue);
+				}
 			}
 		}
 
