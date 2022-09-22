90b064c742b135d64c8fbe8da87d6c264dc1042d
Rubén Pulido <ruben.pulido@liferay.com>
2022-09-22 15:52:44 +0200

    CHESS-30 Display fixed chess board with pieces in initial position

--- modules/apps/chess/chess-web/src/main/resources/META-INF/resources/js/chess_play_game/ChessBoard.js
+++ modules/apps/chess/chess-web/src/main/resources/META-INF/resources/js/chess_play_game/ChessBoard.js
@@ -15,5 +15,159 @@
 import React from 'react';
 
 export default function ChessBoard() {
-	return <div className="chess-game__chess-board">Chess Board</div>;
+	return (
+		<div>
+			Chess Board
+			<div>
+				<table>
+					<tbody>
+						<tr>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/rook_black.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/knight_black.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/bishop_black.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/queen_black.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/king_black.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/bishop_black.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/knight_black.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/rook_black.svg" />
+							</td>
+						</tr>
+						<tr>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/black/pawn_black.svg" />
+							</td>
+						</tr>
+						<tr>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+						</tr>
+						<tr>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+						</tr>
+						<tr>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+						</tr>
+						<tr>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+							<td className="square-black"></td>
+							<td className="square-white"></td>
+						</tr>
+						<tr>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/pawn_white.svg" />
+							</td>
+						</tr>
+						<tr>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/rook_white.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/knight_white.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/bishop_white.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/queen_white.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/king_white.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/bishop_white.svg" />
+							</td>
+							<td className="square-black">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/knight_white.svg" />
+							</td>
+							<td className="square-white">
+								<img src="/o/chess-web/images/chess_pieces/cburnett/white/rook_white.svg" />
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 }
