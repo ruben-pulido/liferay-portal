9763a96c135ba5a572d110b91abd1ff34c5292c7
Rubén Pulido <ruben.pulido@liferay.com>
2022-12-21 11:09:07 +0100

    LPS-169503 Perform a fetch when deleting a page and navigate to the redirect location.
    Co-authored-by: Victor Galan <victor.galan@liferay.com>

--- modules/apps/product-navigation/product-navigation-product-menu-web/src/main/resources/META-INF/resources/js/PagesTree.js
+++ modules/apps/product-navigation/product-navigation-product-menu-web/src/main/resources/META-INF/resources/js/PagesTree.js
@@ -288,7 +288,16 @@ function normalizeActions(actions, namespace) {
 									onClick: ({processClose}) => {
 										processClose();
 
-										navigate(item.data.url);
+									fetch(item.data.url, {
+										method: 'post',
+										})
+									.then(
+										(response) => {
+											if (response.redirected) {
+												navigate(response.url);
+											}
+										}
+									).catch(() => openErrorToast());
 									},
 								},
 							],
