520d0ac5d8d229ffa31468567a68d7a97b9b98d1
Sandro Chinea <sandrochinea@gmail.com>
2022-12-22 15:55:40 +0000

    LPS-169503 Make copy modal smaller

--- modules/apps/product-navigation/product-navigation-product-menu-web/src/main/resources/META-INF/resources/js/PagesTree.js
+++ modules/apps/product-navigation/product-navigation-product-menu-web/src/main/resources/META-INF/resources/js/PagesTree.js
@@ -311,7 +311,11 @@ function normalizeActions(actions, namespace) {
 					else if (item.id === ACTION_COPY_PAGE) {
 						modalData = {
 							...modalData,
+							containerProps: {
+								className: 'cadmin copy-page-modal',
+							},
 							id: 'addLayoutDialog',
+							size: 'md',
 						};
 					}
 
