d28b1aa7dee7dcb3a1843bf6603244be69b9ef89
Rubén Pulido <ruben.pulido@liferay.com>
2023-01-03 02:18:27 +0100

    LPS-171977 Return paginated as true, only if layout count is greater than the number of layouts

--- modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/display/context/LayoutsTreeDisplayContext.java
+++ modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/display/context/LayoutsTreeDisplayContext.java
@@ -413,6 +413,8 @@ public class LayoutsTreeDisplayContext {
 			layoutsJSONArray = JSONFactoryUtil.createJSONArray();
 		}
 
+		int numberOfLayouts = layoutsJSONArray.length();
+
 		return JSONUtil.putAll(
 			JSONUtil.put(
 				"children", layoutsJSONArray
@@ -429,8 +431,9 @@ public class LayoutsTreeDisplayContext {
 						_groupId, _isPrivateLayout(),
 						LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
 
-					if (layoutsCount >
-							PropsValues.LAYOUT_MANAGE_PAGES_INITIAL_CHILDREN) {
+					if ((layoutsCount >
+							PropsValues.LAYOUT_MANAGE_PAGES_INITIAL_CHILDREN) &&
+						(layoutsCount > numberOfLayouts)) {
 
 						return true;
 					}
