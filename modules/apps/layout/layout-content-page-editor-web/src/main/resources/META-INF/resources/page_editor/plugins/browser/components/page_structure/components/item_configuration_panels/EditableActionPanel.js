01121e5dfca3b461d0fe7412c16719f9954dac02
Sandro Chinea <sandrochinea@gmail.com>
2023-05-25 15:16:16 +0200

    3 WIP Action Selector

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/browser/components/page_structure/components/item_configuration_panels/EditableActionPanel.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/browser/components/page_structure/components/item_configuration_panels/EditableActionPanel.js
@@ -14,6 +14,57 @@
 
 import React from 'react';
 
-export default function EditableActionPanel() {
-	return <></>;
+import {EDITABLE_FRAGMENT_ENTRY_PROCESSOR} from '../../../../../../app/config/constants/editableFragmentEntryProcessor';
+import {EDITABLE_TYPES} from '../../../../../../app/config/constants/editableTypes';
+import {
+	useDispatch,
+	useSelectorCallback,
+} from '../../../../../../app/contexts/StoreContext';
+import selectEditableValues from '../../../../../../app/selectors/selectEditableValues';
+import updateEditableValues from '../../../../../../app/thunks/updateEditableValues';
+import {updateIn} from '../../../../../../app/utils/updateIn';
+import MappingSelector from '../../../../../../common/components/MappingSelector';
+import {getEditableItemPropTypes} from '../../../../../../prop_types/index';
+
+export default function EditableActionPanel({item}) {
+	const dispatch = useDispatch();
+
+	const editableValues = useSelectorCallback(
+		(state) => selectEditableValues(state, item.fragmentEntryLinkId),
+		[item.fragmentEntryLinkId]
+	);
+
+	const onValueSelect = (name, value) => {
+		dispatch(
+			updateEditableValues({
+				editableValues: updateIn(
+					editableValues,
+					[
+						EDITABLE_FRAGMENT_ENTRY_PROCESSOR,
+						[item.editableId],
+						'config',
+						name,
+					],
+					() => value
+				),
+				fragmentEntryLinkId: item.fragmentEntryLinkId,
+			})
+		);
+	};
+
+	return (
+		<>
+			<MappingSelector
+				fieldType={EDITABLE_TYPES.objectAction}
+				mappedItem={editableValues.config?.mappedAction || {}}
+				onMappingSelect={(action) => {
+					onValueSelect('mappedAction', action);
+				}}
+			/>
+		</>
+	);
 }
+
+EditableActionPanel.propTypes = {
+	item: getEditableItemPropTypes(),
+};
