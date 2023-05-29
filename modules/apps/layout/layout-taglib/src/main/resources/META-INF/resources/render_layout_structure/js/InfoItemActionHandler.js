36b9e7a89de909416fb669ccd826138a324579bc
Sandro Chinea <sandrochinea@gmail.com>
2023-05-29 14:05:16 +0200

    LPS-185604 Add js component to handle object action triggering

---
+++ modules/apps/layout/layout-taglib/src/main/resources/META-INF/resources/render_layout_structure/js/InfoItemActionHandler.js
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {objectToFormData, openToast} from 'frontend-js-web';
+
+export default function ({executeInfoItemActionURL}) {
+	const triggers = document.querySelectorAll(
+		'[data-lfr-editable-type="action"]'
+	);
+
+	const onClick = (event) => {
+		triggerAction(event.target, executeInfoItemActionURL);
+	};
+
+	triggers.forEach((trigger) => {
+		trigger.addEventListener('click', onClick);
+	});
+
+	return {
+		dispose() {
+			triggers.forEach((trigger) => {
+				trigger.removeEventListener('click', onClick);
+			});
+		},
+	};
+}
+
+function triggerAction(trigger, executeInfoItemActionURL) {
+	const {
+		lfrClassNameId: classNameId,
+		lfrClassPk: classPK,
+		lfrFieldId: fieldId,
+	} = trigger.dataset;
+
+	if (!fieldId) {
+		return;
+	}
+
+	const loadingIndicator = getLoadingIndicator();
+
+	trigger.classList.add('disabled');
+	trigger.appendChild(loadingIndicator);
+
+	Liferay.Util.fetch(new URL(executeInfoItemActionURL), {
+		body: objectToFormData({
+			classNameId,
+			classPK,
+			fieldId,
+		}),
+		method: 'POST',
+	})
+		.then(() => {
+			trigger.classList.remove('disabled');
+			trigger.removeChild(loadingIndicator);
+
+			openResultToast('success');
+		})
+		.catch(() => {
+			trigger.classList.remove('disabled');
+			trigger.removeChild(loadingIndicator);
+
+			openResultToast('danger');
+		});
+}
+
+function getLoadingIndicator() {
+	const element = document.createElement('span');
+
+	element.classList.add(
+		'd-inline-block',
+		'loading-animation',
+		'loading-animation-light',
+		'loading-animation-sm',
+		'ml-2',
+		'my-0'
+	);
+
+	return element;
+}
+
+function openResultToast(type) {
+	openToast({
+		message:
+			type === 'success'
+				? Liferay.Language.get('your-request-completed-successfully')
+				: Liferay.Language.get('your-request-failed-to-complete'),
+		title:
+			type === 'success'
+				? Liferay.Language.get('success')
+				: Liferay.Language.get('error'),
+		type,
+	});
+}
