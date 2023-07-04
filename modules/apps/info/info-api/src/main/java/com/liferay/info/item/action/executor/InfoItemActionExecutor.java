987b670235a45c368b3d82bff93506b08422ed1d
Rubén Pulido <ruben.pulido@liferay.com>
2023-07-04 15:27:11 +0200

    LPS-189594 Add getInfoItemActionErrorMessage to InfoItemActionExecutor interface

--- modules/apps/info/info-api/src/main/java/com/liferay/info/item/action/executor/InfoItemActionExecutor.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/item/action/executor/InfoItemActionExecutor.java
@@ -14,6 +14,7 @@
 
 package com.liferay.info.item.action.executor;
 
+import com.liferay.info.exception.InfoItemActionErrorMessageException;
 import com.liferay.info.exception.InfoItemActionExecutionException;
 import com.liferay.info.item.InfoItemIdentifier;
 
@@ -25,5 +26,8 @@ public interface InfoItemActionExecutor<T> {
 	public void executeInfoItemAction(
 			InfoItemIdentifier infoItemIdentifier, String fieldId)
 		throws InfoItemActionExecutionException;
+
+	public String getInfoItemActionErrorMessage(String fieldId)
+		throws InfoItemActionErrorMessageException;
 
 }
