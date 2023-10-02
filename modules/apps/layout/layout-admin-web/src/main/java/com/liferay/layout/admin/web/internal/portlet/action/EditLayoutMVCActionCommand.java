ca3d199ded322af4ce6870591b68a9e030c49f7b
Rubén Pulido <ruben.pulido@liferay.com>
2023-10-03 00:10:47 +0200

    LPS-197543 Escape redirect

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutMVCActionCommand.java
@@ -281,7 +281,8 @@ public class EditLayoutMVCActionCommand extends BaseMVCActionCommand {
 			stagingGroupId, privateLayout, layout.getLayoutId(),
 			layout.getTypeSettingsProperties());
 
-		String redirect = ParamUtil.getString(actionRequest, "redirect");
+		String redirect = _portal.escapeRedirect(
+			ParamUtil.getString(actionRequest, "redirect"));
 
 		if (Validator.isNull(redirect) ||
 			(redirect.contains(oldFriendlyURL) &&
