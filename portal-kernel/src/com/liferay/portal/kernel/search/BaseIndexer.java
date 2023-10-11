0851d04698a502dfce14d3a0e57ebfc0db727bdc
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-10-11 11:47:32 +0200

    LPS-196807 Use the new method to get the entryClassPK from the base model
    Calculates correctly the entryClassPK by delegating the calculation to the asset renderer factory. Also, consider that the asset entry could not exist, for example, in the case of published versions that are not the latest ones.
    
    The entry class PK of the resource models does not always match the resourcePrimKey. There could be more than one asset entry from the same article, which happens in the case of non-visible versions (draft, scheduled, pending...).
    
    In this scenario, only the last approved version, or the first one in case no published version, will use the article resourcePrimKey as her asset entryClassPK. The other non-visible versions will use its ID as entryClassPK.

--- portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
@@ -5,7 +5,9 @@
 
 package com.liferay.portal.kernel.search;
 
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
 import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
 import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
@@ -1138,7 +1140,8 @@ public abstract class BaseIndexer<T> implements Indexer<T> {
 
 		DocumentHelper documentHelper = new DocumentHelper(document);
 
-		documentHelper.setEntryKey(className, classPK);
+		documentHelper.setEntryKey(
+			className, _getEntryClassPK(baseModel, className, classPK));
 
 		document.addUID(className, classPK);
 
@@ -1449,6 +1452,33 @@ public abstract class BaseIndexer<T> implements Indexer<T> {
 		}
 
 		return entryClassNameIndexerMap;
+	}
+
+	private <T> long _getEntryClassPK(T entry, String className, long classPK) {
+		AssetRendererFactory<T> assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
+				className);
+
+		if (assetRendererFactory == null) {
+			return classPK;
+		}
+
+		try {
+			AssetEntry assetEntry = assetRendererFactory.getAssetEntry(entry);
+
+			if (assetEntry != null) {
+				return assetEntry.getClassPK();
+			}
+
+			return 0;
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException);
+			}
+		}
+
+		return classPK;
 	}
 
 	private SearchResultPermissionFilter _getSearchResultPermissionFilter(
