45bb7411ffe74425b343b355a9b355dcf37f86a2
Rubén Pulido <ruben.pulido@liferay.com>
2023-11-08 16:15:44 +0100

    LPS-201091 If "recent" filter has been applied, do not display "mine" as available filter, since it is already implicitly applied as part of "recent".

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalManagementToolbarDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalManagementToolbarDisplayContext.java
@@ -580,18 +580,20 @@ public class JournalManagementToolbarDisplayContext
 				LanguageUtil.get(httpServletRequest, "all")
 			).build());
 
-		filterNavigationDropdownItems.add(
-			DropdownItemBuilder.setActive(
-				_journalDisplayContext.isNavigationMine()
-			).setHref(
-				PortletURLBuilder.create(
-					getPortletURL()
-				).setParameter(
-					"navigationMine", Boolean.TRUE
-				).buildPortletURL()
-			).setLabel(
-				LanguageUtil.get(httpServletRequest, "mine")
-			).build());
+		if (!_journalDisplayContext.isNavigationRecent()) {
+			filterNavigationDropdownItems.add(
+				DropdownItemBuilder.setActive(
+					_journalDisplayContext.isNavigationMine()
+				).setHref(
+					PortletURLBuilder.create(
+						getPortletURL()
+					).setParameter(
+						"navigationMine", Boolean.TRUE
+					).buildPortletURL()
+				).setLabel(
+					LanguageUtil.get(httpServletRequest, "mine")
+				).build());
+		}
 
 		filterNavigationDropdownItems.add(
 			DropdownItemBuilder.setActive(
