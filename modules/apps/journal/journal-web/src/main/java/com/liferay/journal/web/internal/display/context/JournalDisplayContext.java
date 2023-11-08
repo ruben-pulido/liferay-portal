d1fdae2c0a47098806f5a3f018939c468ea87ec2
Rubén Pulido <ruben.pulido@liferay.com>
2023-11-08 15:57:13 +0100

    LPS-201091 Take into account new attributes when obtaining PortletURL

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -920,6 +920,12 @@ public class JournalDisplayContext {
 				"navigation", HtmlUtil.escapeJS(getNavigation()));
 		}
 
+		portletURL.setParameter(
+			"navigationMine", String.valueOf(isNavigationMine()));
+
+		portletURL.setParameter(
+			"navigationRecent", String.valueOf(isNavigationRecent()));
+
 		portletURL.setParameter("folderId", String.valueOf(getFolderId()));
 
 		if (isNavigationStructure()) {
