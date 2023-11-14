42a69e3941ef4f232d29b10678bbfa7124fe8db9
Rubén Pulido <ruben.pulido@liferay.com>
2023-11-14 02:29:49 +0100

    LPS-201091 isSearch should only return true if keyword search is being performed (and not if category or tag filters are being applied)

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1328,10 +1328,7 @@ public class JournalDisplayContext {
 	}
 
 	public boolean isSearch() {
-		if (Validator.isNotNull(getKeywords()) ||
-			ArrayUtil.isNotEmpty(_getAssetCategoryIds()) ||
-			ArrayUtil.isNotEmpty(_getAssetTagNames())) {
-
+		if (Validator.isNotNull(getKeywords())) {
 			return true;
 		}
 
