e4a05944b2f34f6a3ac059c80f143240dc0da5ef
Rubén Pulido <ruben.pulido@liferay.com>
2024-04-18 11:45:32 +0200

    LPD-21554 Use soft assertions so if any of them fail, the test is not terminated and the page can be deleted at the end of the test

--- modules/test/playwright/tests/layout-admin-web/pageFriendlyURL.spec.ts
+++ modules/test/playwright/tests/layout-admin-web/pageFriendlyURL.spec.ts
@@ -46,11 +46,11 @@ test('This is a test for LPD-21554. Some page names result in 404 friendly URLs.
 		title: pageName,
 	});
 
-	await expect(sitePage.friendlyUrlPath).toBe(`/${pageName}`);
+	await expect.soft(sitePage.friendlyUrlPath).toBe(`/${pageName}`);
 
 	await page.goto(sitePage.friendlyUrlPath);
 
-	await expect(page.getByText('Heading Example')).toBeVisible();
+	await expect.soft(page.getByText('Heading Example')).toBeVisible();
 
 	await apiHelpers.jsonWebServicesLayout.deleteLayout(sitePage.id);
 });
