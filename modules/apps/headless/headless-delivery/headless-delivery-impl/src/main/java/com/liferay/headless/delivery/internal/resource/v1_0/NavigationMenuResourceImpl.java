a5b84106d295f5efbbe90e55713964d9e741b0d3
Rubén Pulido <ruben.pulido@liferay.com>
2024-07-17 15:48:44 +0200

    LPD-X Delete also child navigation menu items when deleting a navigation item during putNavigationMenu

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/NavigationMenuResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/NavigationMenuResourceImpl.java
@@ -763,7 +763,7 @@ public class NavigationMenuResourceImpl extends BaseNavigationMenuResourceImpl {
 				siteNavigationMenuItems) {
 
 			_siteNavigationMenuItemService.deleteSiteNavigationMenuItem(
-				siteNavigationMenuItem.getSiteNavigationMenuItemId());
+				siteNavigationMenuItem.getSiteNavigationMenuItemId(), true);
 		}
 	}
 
