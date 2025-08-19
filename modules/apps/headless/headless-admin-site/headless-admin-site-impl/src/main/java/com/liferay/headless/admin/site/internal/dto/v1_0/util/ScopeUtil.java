4cbbdce8e9895ba8235173991ea14fa3cd31f29c
Rubén Pulido <ruben.pulido@liferay.com>
2025-08-19 13:52:31 +0200

    LPD-55438 Extract getScope method to ScopeUtil

---
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/util/ScopeUtil.java
@@ -0,0 +1,42 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.site.internal.dto.v1_0.util;
+
+import com.liferay.headless.admin.site.dto.v1_0.Scope;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+
+/**
+ * @author Rubén Pulido
+ */
+public class ScopeUtil {
+
+	public static Scope getScope(long groupId, long scopeGroupId)
+		throws Exception {
+
+		if (groupId == scopeGroupId) {
+			return null;
+		}
+
+		Group group = GroupLocalServiceUtil.getGroup(scopeGroupId);
+
+		return new Scope() {
+			{
+				setExternalReferenceCode(group::getExternalReferenceCode);
+				setType(
+					() -> {
+						if (group.getType() == GroupConstants.TYPE_DEPOT) {
+							return Type.ASSET_LIBRARY;
+						}
+
+						return Type.SITE;
+					});
+			}
+		};
+	}
+
+}
