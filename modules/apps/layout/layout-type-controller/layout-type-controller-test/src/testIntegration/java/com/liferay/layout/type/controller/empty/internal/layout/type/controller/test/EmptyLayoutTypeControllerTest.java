19f7a8b912fcf51741f788aa22e2cf15a17a9cdb
Cheryl Tang <cheryl.tang@liferay.com>
2025-09-17 13:36:23 +0200

    LPD-62538 Fix test case to actually try to navigate to the empty layout's friendly URL and for new behavior

--- modules/apps/layout/layout-type-controller/layout-type-controller-test/src/testIntegration/java/com/liferay/layout/type/controller/empty/internal/layout/type/controller/test/EmptyLayoutTypeControllerTest.java
+++ modules/apps/layout/layout-type-controller/layout-type-controller-test/src/testIntegration/java/com/liferay/layout/type/controller/empty/internal/layout/type/controller/test/EmptyLayoutTypeControllerTest.java
@@ -74,8 +74,8 @@ public class EmptyLayoutTypeControllerTest {
 			null, TestPropsValues.getUserId(), _group.getGroupId(), false,
 			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
 			RandomTestUtil.randomString(), StringPool.BLANK, StringPool.BLANK,
-			LayoutConstants.TYPE_EMPTY, false, StringPool.BLANK,
-			_serviceContext);
+			LayoutConstants.TYPE_EMPTY, false,
+			StringPool.SLASH + RandomTestUtil.randomString(), _serviceContext);
 
 		_layoutTypeController =
 			LayoutTypeControllerTracker.getLayoutTypeController(
@@ -88,11 +88,11 @@ public class EmptyLayoutTypeControllerTest {
 			LayoutConstants.TYPE_EMPTY, _layoutTypeController.getType());
 
 		try {
-			_layoutTypeController.includeLayoutContent(
-				_getMockHttpServletRequest(TestPropsValues.getUser()),
-				new MockHttpServletResponse(), _layout);
-
-			Assert.fail();
+			Assert.assertFalse(
+				_layoutTypeController.includeLayoutContent(
+					_getMockHttpServletRequest(
+						TestPropsValues.getUser(), _layout.getFriendlyURL()),
+					new MockHttpServletResponse(), _layout));
 		}
 		catch (NoSuchLayoutException noSuchLayoutException) {
 			if (_log.isDebugEnabled()) {
@@ -103,18 +103,19 @@ public class EmptyLayoutTypeControllerTest {
 		Assert.assertEquals(
 			StringPool.BLANK,
 			_layoutTypeController.includeEditContent(
-				_getMockHttpServletRequest(TestPropsValues.getUser()),
+				_getMockHttpServletRequest(
+					TestPropsValues.getUser(), _layout.getFriendlyURL()),
 				new MockHttpServletResponse(), _layout));
 	}
 
-	private MockHttpServletRequest _getMockHttpServletRequest(User user)
+	private MockHttpServletRequest _getMockHttpServletRequest(
+			User user, String currentURL)
 		throws Exception {
 
 		MockHttpServletRequest mockHttpServletRequest =
 			new MockHttpServletRequest();
 
-		mockHttpServletRequest.setAttribute(
-			WebKeys.CURRENT_URL, "http://www.liferay.com");
+		mockHttpServletRequest.setAttribute(WebKeys.CURRENT_URL, currentURL);
 
 		UserTestUtil.setUser(user);
 
@@ -151,6 +152,8 @@ public class EmptyLayoutTypeControllerTest {
 		themeDisplay.setServerPort(8080);
 		themeDisplay.setSignedIn(true);
 		themeDisplay.setSiteGroupId(_group.getGroupId());
+		themeDisplay.setURLCurrent(
+			(String)mockHttpServletRequest.getAttribute(WebKeys.CURRENT_URL));
 		themeDisplay.setUser(user);
 
 		return themeDisplay;
