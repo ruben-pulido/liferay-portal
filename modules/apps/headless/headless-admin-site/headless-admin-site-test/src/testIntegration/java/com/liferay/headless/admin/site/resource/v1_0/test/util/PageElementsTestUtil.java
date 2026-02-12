4d882aa476f14b984e6c7f0265a13ea9d12133ed
Rubén Pulido <ruben.pulido@liferay.com>
2026-02-12 12:52:43 +0100

    LPD-77031 Allow specifying hidden and fragmentEditableElements when getting a test BasicFragmentInstancePageElementDefinition

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/util/PageElementsTestUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/util/PageElementsTestUtil.java
@@ -240,6 +240,7 @@ public class PageElementsTestUtil {
 		getBasicFragmentInstancePageElementDefinition(
 			BackgroundImageValue backgroundImageValue,
 			Map<String, Object> configurationValuesMap, String key,
+			FragmentEditableElement[] fragmentEditableElements, Boolean hidden,
 			long scopeGroupId) {
 
 		FragmentEntry fragmentEntry =
@@ -248,7 +249,7 @@ public class PageElementsTestUtil {
 		if (fragmentEntry != null) {
 			return getBasicFragmentInstancePageElementDefinition(
 				backgroundImageValue, configurationValuesMap,
-				new FragmentEditableElement[0], fragmentEntry, scopeGroupId);
+				fragmentEditableElements, fragmentEntry, hidden, scopeGroupId);
 		}
 
 		FragmentRenderer fragmentRenderer =
@@ -257,10 +258,22 @@ public class PageElementsTestUtil {
 		if (fragmentRenderer != null) {
 			return getBasicFragmentInstancePageElementDefinition(
 				backgroundImageValue, configurationValuesMap,
-				new FragmentEditableElement[0], fragmentRenderer, scopeGroupId);
+				fragmentEditableElements, fragmentRenderer, scopeGroupId);
 		}
 
 		return null;
+	}
+
+	public static BasicFragmentInstancePageElementDefinition
+		getBasicFragmentInstancePageElementDefinition(
+			BackgroundImageValue backgroundImageValue,
+			Map<String, Object> configurationValuesMap, String key,
+			long scopeGroupId) {
+
+		return getBasicFragmentInstancePageElementDefinition(
+			backgroundImageValue, configurationValuesMap, key,
+			new FragmentEditableElement[0], RandomTestUtil.randomBoolean(),
+			scopeGroupId);
 	}
 
 	public static PageElement getDropZonePageElement(
