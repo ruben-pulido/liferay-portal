5628b0b544718ad908d821d67fc5d5c09eadc5e1
Rubén Pulido <ruben.pulido@liferay.com>
2026-05-08 00:47:42 +0200

    Revert "LPD-88748 WIP - FragmentEntry staging"
    This reverts commit f321db20e5fcdf35cede063a702c83423b1c77c4.

--- modules/apps/headless/headless-admin-fragment/headless-admin-fragment-impl/src/main/java/com/liferay/headless/admin/fragment/internal/resource/v1_0/FragmentResourceImpl.java
+++ modules/apps/headless/headless-admin-fragment/headless-admin-fragment-impl/src/main/java/com/liferay/headless/admin/fragment/internal/resource/v1_0/FragmentResourceImpl.java
@@ -5,7 +5,6 @@
 
 package com.liferay.headless.admin.fragment.internal.resource.v1_0;
 
-import com.liferay.exportimport.vulcan.batch.engine.ExportImportVulcanBatchEngineTaskItemDelegate;
 import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.fragment.constants.FragmentPortletKeys;
 import com.liferay.fragment.exception.RequiredFragmentEntryVersionException;
@@ -41,9 +40,6 @@ import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 
-import java.util.List;
-import java.util.function.Function;
-
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
@@ -53,12 +49,9 @@ import org.osgi.service.component.annotations.ServiceScope;
  */
 @Component(
 	properties = "OSGI-INF/liferay/rest/v1_0/fragment.properties",
-	property = "export.import.vulcan.batch.engine.task.item.delegate=true",
 	scope = ServiceScope.PROTOTYPE, service = FragmentResource.class
 )
-public class FragmentResourceImpl
-	extends BaseFragmentResourceImpl
-	implements ExportImportVulcanBatchEngineTaskItemDelegate<Fragment> {
+public class FragmentResourceImpl extends BaseFragmentResourceImpl {
 
 	@Override
 	public void deleteSiteFragment(
@@ -73,56 +66,6 @@ public class FragmentResourceImpl
 			GroupUtil.getStagingAwareGroupId(
 				true, contextCompany.getCompanyId(),
 				siteExternalReferenceCode));
-	}
-
-	@Override
-	public ExportImportDescriptor<FragmentEntry> getExportImportDescriptor() {
-		return new ExportImportDescriptor<FragmentEntry>() {
-
-			@Override
-			public Function<FragmentEntry, Boolean>
-				getApplicableModelFunction() {
-
-				return fragmentEntry ->
-					fragmentEntry.getType() == FragmentConstants.TYPE_COMPONENT;
-			}
-
-			@Override
-			public String getKey() {
-				return FragmentResourceImpl.class.getName();
-			}
-
-			@Override
-			public String getLabelLanguageKey() {
-				return "fragments";
-			}
-
-			@Override
-			public Class<FragmentEntry> getModelClass() {
-				return FragmentEntry.class;
-			}
-
-			@Override
-			public List<String> getNestedFields() {
-				return List.of("thumbnail");
-			}
-
-			@Override
-			public String getPortletId() {
-				return FragmentPortletKeys.FRAGMENT;
-			}
-
-			@Override
-			public Scope getScope() {
-				return Scope.SITE;
-			}
-
-			@Override
-			public boolean isStagingSupported() {
-				return true;
-			}
-
-		};
 	}
 
 	@Override
