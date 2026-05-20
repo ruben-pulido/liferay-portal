7061eb68e628b762023fc5da301953488829742d
Rubén Pulido <ruben.pulido@liferay.com>
2026-05-20 17:31:59 +0200

    LPD-75909 wip

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/change/tracking/spi/listener/FragmentEntryVersionCTEventListener.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/change/tracking/spi/listener/FragmentEntryVersionCTEventListener.java
@@ -11,8 +11,18 @@ import com.liferay.fragment.model.FragmentEntryVersion;
 import com.liferay.fragment.model.FragmentEntryVersionTable;
 import com.liferay.fragment.service.persistence.FragmentEntryVersionPersistence;
 import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
+import com.liferay.portal.kernel.transaction.TransactionConfig;
+import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import java.util.List;
 
@@ -27,28 +37,77 @@ public class FragmentEntryVersionCTEventListener implements CTEventListener {
 
 	@Override
 	public void onAfterPublish(long ctCollectionId) {
-		try {
-			List<Long> fragmentEntryIds =
-				_fragmentEntryVersionPersistence.dslQuery(
-					DSLQueryFactoryUtil.selectDistinct(
-						FragmentEntryVersionTable.INSTANCE.fragmentEntryId
-					).from(
-						FragmentEntryVersionTable.INSTANCE
-					).where(
-						FragmentEntryVersionTable.INSTANCE.ctCollectionId.eq(0L)
-					));
+		System.err.println(
+			"LPD-75909 onAfterPublish fired ctCollectionId=" + ctCollectionId +
+				" threadLocalCT=" +
+					CTCollectionThreadLocal.getCTCollectionId());
 
-			for (long fragmentEntryId : fragmentEntryIds) {
-				_trimProductionVersions(fragmentEntryId);
+		_dumpRawCounts("onAfterPublish entry (pre-commit)");
+
+		TransactionCommitCallbackUtil.registerCallback(
+			() -> {
+				_dumpRawCounts("commit callback (post-commit)");
+
+				try {
+					TransactionInvokerUtil.invoke(
+						_transactionConfig,
+						() -> {
+							_fragmentEntryVersionPersistence.clearCache();
+
+							List<Long> fragmentEntryIds =
+								_fragmentEntryVersionPersistence.dslQuery(
+									DSLQueryFactoryUtil.selectDistinct(
+										FragmentEntryVersionTable.INSTANCE.
+											fragmentEntryId
+									).from(
+										FragmentEntryVersionTable.INSTANCE
+									).where(
+										FragmentEntryVersionTable.INSTANCE.
+											ctCollectionId.eq(0L)
+									));
+
+							System.err.println(
+								"LPD-75909 commit callback found " +
+									"fragmentEntryIds=" + fragmentEntryIds);
+
+							for (long fragmentEntryId : fragmentEntryIds) {
+								_trimProductionVersions(fragmentEntryId);
+							}
+
+							return null;
+						});
+				}
+				catch (Throwable throwable) {
+					System.err.println(
+						"LPD-75909 commit callback FAILED for ctCollectionId=" +
+							ctCollectionId);
+					throwable.printStackTrace();
+				}
+
+				return null;
+			});
+	}
+
+	private void _dumpRawCounts(String label) {
+		try (Connection connection = DataAccess.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(
+				"select ctCollectionId, count(*) c from FragmentEntryVersion " +
+					"where fragmentEntryId=102 group by ctCollectionId order " +
+						"by ctCollectionId");
+			ResultSet resultSet = preparedStatement.executeQuery()) {
+
+			StringBuilder sb = new StringBuilder(
+				"LPD-75909 RAW @ " + label + " ->");
+
+			while (resultSet.next()) {
+				sb.append(
+					" ct=" + resultSet.getLong(1) + ":" + resultSet.getInt(2));
 			}
+
+			System.err.println(sb);
 		}
 		catch (Exception exception) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Unable to clean up old fragment entry versions after " +
-						"publishing CT collection ID " + ctCollectionId,
-					exception);
-			}
+			System.err.println("LPD-75909 RAW dump failed: " + exception);
 		}
 	}
 
@@ -66,6 +125,10 @@ public class FragmentEntryVersionCTEventListener implements CTEventListener {
 							fragmentEntryId)
 					)
 				));
+
+			System.err.println(
+				"LPD-75909 _trimProductionVersions fragmentEntryId=" +
+					fragmentEntryId + " versionCount=" + versionCount);
 
 			if (versionCount <=
 					FragmentEntryVersionModelListener.MAX_VERSIONS) {
@@ -93,20 +156,30 @@ public class FragmentEntryVersionCTEventListener implements CTEventListener {
 						versionCount
 					));
 
+			System.err.println(
+				"LPD-75909 _trimProductionVersions deleting " +
+					fragmentEntryVersions.size() + " rows for fragmentEntryId=" +
+					fragmentEntryId);
+
 			for (FragmentEntryVersion fragmentEntryVersionToDelete :
 					fragmentEntryVersions) {
+
+				System.err.println(
+					"LPD-75909 _trimProductionVersions removing version=" +
+						fragmentEntryVersionToDelete.getVersion() +
+							" fragmentEntryVersionId=" +
+								fragmentEntryVersionToDelete.
+									getFragmentEntryVersionId());
 
 				_fragmentEntryVersionPersistence.remove(
 					fragmentEntryVersionToDelete);
 			}
 		}
 		catch (Exception exception) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Unable to clean up old fragment entry versions for " +
-						"fragment entry ID " + fragmentEntryId,
-					exception);
-			}
+			System.err.println(
+				"LPD-75909 _trimProductionVersions FAILED fragmentEntryId=" +
+					fragmentEntryId);
+			exception.printStackTrace();
 		}
 	}
 
@@ -115,5 +188,9 @@ public class FragmentEntryVersionCTEventListener implements CTEventListener {
 
 	@Reference
 	private FragmentEntryVersionPersistence _fragmentEntryVersionPersistence;
+
+	private final TransactionConfig _transactionConfig =
+		TransactionConfig.Factory.create(
+			Propagation.REQUIRES_NEW, new Class<?>[] {Exception.class});
 
 }
